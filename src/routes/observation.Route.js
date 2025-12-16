4fa7561f8c4d2fa39c4e83a4cbf88a5794447f59
shayan9-dev <shayanahmed8862@gmail.com>
2025-12-16 21:59:04 +0500

    fixed

--- src/routes/observation.Route.js
+++ src/routes/observation.Route.js
@@ -1,19 +1,14 @@
 import express from "express";
-import auth from "../middleware/auth.middleware.js";
-import {
-  createObservation,
-  getMyObservations,
-  getObservationById,
-  updateObservation,
-  deleteObservation
-} from "../controllers/observation.controller.js";
+import { createObservation, deleteObservation, getMyObservations, getObservationById, updateObservation } from "../controllers/observation.Controller.js";
+import { authenticate } from "../middleware/authMiddleware.js";
+
 
 const router = express.Router();
 
-router.post("/", auth, createObservation);
-router.get("/", auth, getMyObservations);
-router.get("/:id", auth, getObservationById);
-router.put("/:id", auth, updateObservation);
-router.delete("/:id", auth, deleteObservation);
+router.post("/", authenticate, createObservation);
+router.get("/", authenticate, getMyObservations);
+router.get("/:id", authenticate, getObservationById);
+router.put("/:id", authenticate, updateObservation);
+router.delete("/:id", authenticate, deleteObservation);
 
 export default router;
